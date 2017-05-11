d40c69b679477bf5e2ca993e1d294c69878f807d
devjin0617 <devjin0617@gmail.com>
2017-05-12 00:01:46 +0900

    Set disable spell checker

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -20,7 +20,8 @@ export default {
     const options = {
       element: document.getElementById('note-editor'),
       toolbar: ['bold', 'italic', 'heading', '|', 'code', 'quote', '|', 'unordered-list', 'ordered-list', '|', 'clean-block', '|', 'link', 'image', 'table', '|', 'preview'],
-      placeholder: '여기에 글을 작성해주세요'
+      placeholder: '여기에 글을 작성해주세요',
+      spellChecker: false
     }
     let simplemde = new this.SimpleMDE(options)
     simplemde.value('')
