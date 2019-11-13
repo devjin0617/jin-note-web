9c0641ca68c44cb4018eb384fb6693cfcf3136b2
developerjin <devjin0617@gmail.com>
2019-11-14 00:54:27 +0900

    Upload component files (error files)

---
+++ src/components/Editor.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <textarea id="note-editor" v-model="text"></textarea>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'note-editor',
+  data() {
+    return {
+      text: '',
+    };
+  },
+  mounted() {
+    const options = {
+      element: document.getElementById('note-editor'),
+      toolbar: ['bold', 'italic', 'heading', '|', 'code', 'quote', '|', 'unordered-list', 'ordered-list', '|', 'clean-block', '|', 'link', 'image', 'table', '|', 'preview'],
+      placeholder: '여기에 글을 작성해주세요',
+      spellChecker: false,
+    };
+    const simplemde = new this.SimpleMDE(options);
+    simplemde.value('');
+  },
+};
+</script>
+
+<style lang="scss">
+.cm-quote-1 {
+  color:gray;
+}
+
+.editor {
+  > div {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    .editor-toolbar {
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+    }
+
+    .CodeMirror {
+      flex:1;
+    }
+  }
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <note-list-item v-for="i in 20" :key="i"></note-list-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ListItem from './ListItem'
+;
+export default {
+  name: 'note-list',
+  created() {
+
+  },
+  components: {
+    'note-list-item': ListItem,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+</style>
